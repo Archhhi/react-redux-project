8e2c34bcac0978c4c747b32f4e2b02d90a70dd1b
Artur <brabus-20@mail.ru>
2020-06-27 14:10:34 +0300

    Updates

---
+++ precache-manifest.c57e3efbf5a3b9548b03f27a4dc270bb.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0163ff9446670fd5c78f11d2a50bfaa",
+    "url": "/react-redux-project/index.html"
+  },
+  {
+    "revision": "9dc4990106e3a9c242a9",
+    "url": "/react-redux-project/static/css/4.d574919e.chunk.css"
+  },
+  {
+    "revision": "1adb370512bc30cc9fad",
+    "url": "/react-redux-project/static/css/5.2c51fc35.chunk.css"
+  },
+  {
+    "revision": "5be089a71cc9f0018808",
+    "url": "/react-redux-project/static/css/6.928e7b5c.chunk.css"
+  },
+  {
+    "revision": "29a13a9e0e0a54a931ac",
+    "url": "/react-redux-project/static/css/main.e180efd6.chunk.css"
+  },
+  {
+    "revision": "acd568fcfeb20f0625f1",
+    "url": "/react-redux-project/static/js/0.0901d8e5.chunk.js"
+  },
+  {
+    "revision": "27cd5ea27e68e77a3f55",
+    "url": "/react-redux-project/static/js/3.4d39859c.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/react-redux-project/static/js/3.4d39859c.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9dc4990106e3a9c242a9",
+    "url": "/react-redux-project/static/js/4.ad8b8263.chunk.js"
+  },
+  {
+    "revision": "1adb370512bc30cc9fad",
+    "url": "/react-redux-project/static/js/5.9a840cc5.chunk.js"
+  },
+  {
+    "revision": "5be089a71cc9f0018808",
+    "url": "/react-redux-project/static/js/6.c2f07fb6.chunk.js"
+  },
+  {
+    "revision": "29a13a9e0e0a54a931ac",
+    "url": "/react-redux-project/static/js/main.415cc20b.chunk.js"
+  },
+  {
+    "revision": "ed7f441a4467e41df7a5",
+    "url": "/react-redux-project/static/js/runtime-main.205a8cd4.js"
+  },
+  {
+    "revision": "2cafed709c18a184f522683c52b2aa14",
+    "url": "/react-redux-project/static/media/35.2cafed70.svg"
+  },
+  {
+    "revision": "1babb79889e226035a55b0803b360726",
+    "url": "/react-redux-project/static/media/archi-15.1babb798.jpg"
+  }
+]);
